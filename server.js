6b6314e489a91edecb2d7da0302c20639088d818
jam-madrigal <jam.madrigal@gmail.com>
2022-03-05 20:15:59 -0800

    Add ssl certificate and key

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+const https = require('https');
 const path = require('path');
 const express = require('express');
 const res = require('express/lib/response');
@@ -14,6 +15,12 @@ app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
-app.listen(PORT, () => {
+https.createServer({
+    key:'key.pem',
+    cert:'cert.pem'
+}).listen(PORT, () => {
     console.log(`Server is listening on port ${PORT}`);
 });
+
+// TO MAKE A CERT: openssl req -x509 -newkey rsa:4096 -nodes -keyout key.pem -out cert.pem 
+// For local certs, just leave everything blank, set common name to your name you want to use
